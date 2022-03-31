04f36d9620c7a7f895a82e2e9d449b7084fde7bf
nastyawb <one.anastasia.two@gmail.com>
2022-03-31 23:37:05 +0300

    data_writer.py edited, more input & output files added

--- configs.py
+++ configs.py
@@ -1,4 +1,5 @@
-input_file_name = '.\input-files\data_5orders.xlsx'
-output_file_name = '.\output-files\schedule_5orders.xlsx'
-deadline = 15 * 24 * 60
+input_file_name = '.\input-files\data.xlsx'
+output_file_name = '.\output-files\schedule.xlsx'
+days = 30
+deadline = days * 24 * 60
 bigM = 100000
--- utils/data_writer.py
+++ utils/data_writer.py
@@ -2,7 +2,7 @@ import plotly.express as px
 import pandas as pd
 import datetime as dt
 from utils.gurobi_model import GRBModel
-from configs import output_file_name
+from configs import output_file_name, days
 
 
 class Writer:
@@ -29,17 +29,18 @@ class Writer:
                 if self.ksi[final_suborder].X > 0 and final_suborder in self.processed_suborders:
                     suborders_of_orders[order].append(final_suborder)
         for order, suborders in suborders_of_orders.items():
-            for subord in suborders:
-                for proc_subord in self.processed_suborders:
-                    if proc_subord not in suborders and proc_subord not in self.grbm.final_subord_id and subord in self.grbm.order_graph[proc_subord]:
-                        suborders.append(proc_subord)
+            if suborders == list(self.grbm.orders[order].keys()):
+                for subord in suborders:
+                    for proc_subord in self.processed_suborders:
+                        if proc_subord not in suborders and proc_subord not in self.grbm.final_subord_id and subord in self.grbm.order_graph[proc_subord]:
+                            suborders.append(proc_subord)
         return suborders_of_orders
 
     def transform_results(self):
         list_of_dicts = []
         for (subord, equip) in self.eps.keys():
             for order, suborders in self.suborders_of_orders.items():
-                if self.eps[(subord, equip)].X > 0 and subord in suborders:
+                if len(suborders) > 4 and self.eps[(subord, equip)].X > 0 and subord in suborders:
                     list_of_dicts.append({'Equipment_ID': str(equip), 'start': self.b[subord].X, 'end': self.c[subord].X,
                                           'Order_ID': str(order), 'Suborder_ID': str(subord)})
         return pd.DataFrame(list_of_dicts)
@@ -48,13 +49,17 @@ class Writer:
         self.done_orders_info['Start'] = dt.datetime(2022, 1, 1) + pd.TimedeltaIndex(self.done_orders_info['start'], unit='m')
         self.done_orders_info['End'] = dt.datetime(2022, 1, 1) + pd.TimedeltaIndex(self.done_orders_info['end'], unit='m')
         self.done_orders_info = self.done_orders_info.drop(columns=['start', 'end'])
-        fig = px.timeline(self.done_orders_info, x_start="Start", x_end="End", y="Equipment_ID", color="Order_ID", text="Suborder_ID")
-        fig.update_xaxes(showgrid=False)
-        fig.update_yaxes(showgrid=True)
-        fig.update_layout(font_size=18)
-        fig.show()
-
         self.done_orders_info.to_excel(output_file_name)
+        # discrete_map_resource = {'233': 'whitesmoke', '234': 'lightgray', '231': 'darkgray', '229': 'dimgray'}
+        # fig = px.timeline(self.done_orders_info, x_start="Start", x_end="End", y="Equipment_ID", color="Order_ID", text="Suborder_ID",
+        #                   color_discrete_map=discrete_map_resource, width=200, height=160)
+        fig = px.timeline(self.done_orders_info, x_start="Start", x_end="End", y="Equipment_ID", color="Order_ID",
+                          title=f'Schedule for {days} days', width=1200, height=900)
+        fig.update_xaxes(showgrid=False)
+        fig.update_yaxes(showgrid=True, gridcolor='whitesmoke')
+        # fig.update_layout(font_size=4, plot_bgcolor='white')
+        fig.update_layout(font_size=16, plot_bgcolor='white')
+        fig.show()
 
 
 if __name__ == '__main__':
